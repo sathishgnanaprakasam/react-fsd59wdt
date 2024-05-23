5d10eafcdffcdbc3c1e93bd48367feb02e146e4e
githubscientist <sathish.datascientist@gmail.com>
2024-05-23 12:03:20 +0530

    creating an array --pushing react elements --rendering arrays

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,18 @@ const App = () => {
     { id: 3, name: 'Mark', age: 40}
   ]
 
+  const listFriends = [];
+  for (let i = 0; i < friends.length; i++){
+    listFriends.push(
+      <li key={friends[i].id}>{friends[i].name} {friends[i].age}</li>
+    )
+  }
+
   return (
     <div>
       <h1>Friends</h1>
       <ul>
-        {
-          friends.map((friend) => 
-            <li key={friend.id}>{friend.name} {friend.age}</li>
-          )
-        }
+        {listFriends}
       </ul>
     </div>
   )
