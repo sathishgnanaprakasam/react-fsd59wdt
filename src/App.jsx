a8ebfecce48bcf6b0ab14c73a8a7c9874f1e72b6
githubscientist <sathish.datascientist@gmail.com>
2024-05-23 11:18:06 +0530

    working with array lits and usage of keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Hello from "./components/Hello";
-
-// Component: App
 const App = () => {
 
-  const name = 'Peter';
+  const friends = [
+    { id: 1, name: 'John', age: 20 },
+    { id: 2, name: 'Peter', age: 30 },
+    { id: 3, name: 'Mark', age: 40}
+  ]
 
   return (
     <div>
-      <Hello 
-        name={name}
-      />
+      <h1>Friends</h1>
+      <ul>
+        {
+          friends.map((friend, index) => 
+            <li key={index}>{friend.name} {friend.age}</li>
+          )
+        }
+      </ul>
     </div>
   )
 }
