b4a5e431acffc9dfbe04b5bbd19bb54b3431c000
githubscientist <sathish.datascientist@gmail.com>
2024-05-27 12:26:22 +0530

    useEffect --without second argument

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
 
   let [count, setCount] = useState(0);
 
+  // runs when the component is mounted
+  // and whenever the state changes (any state within the component)
+  useEffect(() => {
+    console.log('Component mounted');
+  });
+
   const handleIncrement = () => {
     setCount(count + 1);
+  }
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  }
+
+  const handleReset = () => {
+    setCount(0);
   }
 
   return (
     <div>
       <h1>Counter: {count}</h1>
       <button onClick={handleIncrement}>Increment</button>
-      <button>Decrement</button>
-      <button>Reset</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
