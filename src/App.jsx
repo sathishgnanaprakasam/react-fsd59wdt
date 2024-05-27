97fdbd1c1383f67a53d65d9bc397e2d73379329d
githubscientist <sathish.datascientist@gmail.com>
2024-05-27 12:46:09 +0530

    state and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
 import { useEffect, useState } from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
 
   let [count, setCount] = useState(0);
-  let [increment, setIncrement] = useState(0);
-
-  // runs when the component is mounted and count is updated
-  useEffect(() => {
-    console.log('useEffect called');
-    console.log('Count:', count);
-    console.log('Increment:', increment);
-  }, [count]);
 
   const handleIncrement = () => {
-    // setCount(count + 1);
-    setIncrement(increment + 1);
-  }
-
-  const handleDecrement = () => {
-    setCount(count - 1);
-  }
-
-  const handleReset = () => {
-    setCount(0);
+    setCount(count + 1);
   }
 
   return (
     <div>
-      <h1>Counter: {count}</h1>
+      <Counter 
+        count={count}
+      />
       <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
-      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,7 @@
+const Counter = ({ count }) => {
+  return (
+      <h1>Counter: {count}</h1>
+  )
+}
+
+export default Counter;
