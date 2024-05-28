7cc813f8ac1bb36b659844834fb6d24906ef6a95
githubscientist <sathish.datascientist@gmail.com>
2024-05-28 10:32:13 +0530

    passing data from parent to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,22 @@
-import { useState } from "react"
+// passing data from App to Bin component (Parent to Child)
 
-const D = ({ counts }) => {
+const Bin = ({ user }) => {
   return (
     <div>
-      <h2>D</h2>
-      <p>Total counts: { counts.a + counts.b + counts.c + counts.d }</p>
-    </div>
-  )
-}
-
-const C = ({ counts }) => {
-  return (
-    <div>
-      <h2>C</h2>
-      <D 
-        counts={counts}
-      />
-    </div>
-  )
-}
-
-const B = ({ counts }) => {
-  return (
-    <div>
-      <h2>B</h2>
-      <C 
-        counts={counts}
-      />
+      <p>Logged in User: { user }</p>
     </div>
   )
 }
 
 const App = () => {
 
-  const [counts, setCounts] = useState({
-    a: 10,
-    b: 20,
-    c: 30,
-    d: 40
-  });
+  const loggedInUser = 'John Doe';
 
   return (
     <div>
-      <h1>App</h1>
-      <B 
-        counts={counts}
+      <h1>Data from App to Bin</h1>
+      <Bin 
+        user={loggedInUser}
       />
     </div>
   )
