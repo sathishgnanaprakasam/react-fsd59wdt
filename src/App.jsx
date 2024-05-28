d89af538581d09b11f1754aa84b42f45e932fb99
githubscientist <sathish.datascientist@gmail.com>
2024-05-28 12:38:53 +0530

    basic react router dom setup and root route done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-// Component: App
-const App = () => {
-  return <h1>Hello, World!</h1>
-}
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+// create a router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello, World!!</h1>
+  },
+  {
+    path: "/about",
+    element: <h1>About Us</h1>
+  }
+]);
+
+
+const App = () => {
+  // wrap the app with the RouterProvider
+  return <RouterProvider router={router} />;
+}
 export default App;
+
+/*
+  Exercise: 
+
+  1. Create a new route for the path "/contact" with the element <h1>Contact Us</h1>
+  2. Create a new route for the path "/services" with the element <h1>Our Services</h1>
+  3. Create a new route for the path "/products" with the element <h1>Our Products</h1>
+*/
