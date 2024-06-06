b1f131daef544b54a506c611f699dc55cf29e46b
githubscientist <sathish.datascientist@gmail.com>
2024-06-06 11:32:16 +0530

    crud --create

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -18,8 +18,37 @@ const Todos = () => {
     const { todos: initialTodos } = useLoaderData();
     const [todos, setTodos] = useState(initialTodos);
 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [completed, setCompleted] = useState(false);
+
     const handleUpdateTodo = (updatedTodo) => {
         setTodos(todos.map(todo => (todo.id === updatedTodo.id ? updatedTodo : todo)));
+    }
+
+    const handleCreateTodo = async (e) => {
+        e.preventDefault();
+
+        // create a new todo object
+        const newTodo = {
+            title,
+            description,
+            completed
+        }
+
+        // api call to create a new todo
+        const response = await axios.post(`https://665eb6121e9017dc16f10996.mockapi.io/todos`, newTodo);
+
+        if (response) {
+            setTitle('');
+            setDescription('');
+            setCompleted(false);
+
+            // update the state
+            response.data && setTodos([...todos, response.data]);
+
+            alert('Todo created successfully!');
+        }
     }
 
   return (
@@ -33,7 +62,37 @@ const Todos = () => {
                     onUpdateTodo={handleUpdateTodo}
                 />
             ))
-        }
+          }
+          
+          <form onSubmit={handleCreateTodo}>
+              <input 
+                  type="text"
+                  placeholder="Todo title..."
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+              />
+              
+              <input 
+                    type="text"
+                  placeholder="Todo description..."
+                  value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+              />
+
+              <select
+                    value={completed}
+                    onChange={(e) => setCompleted(e.target.value)}
+              >
+                  <option
+                    value={false}
+                  >Not Completed</option>
+                  <option
+                    value={true}
+                  >Completed</option>
+              </select>
+
+                <button type="submit">Add Todo</button>
+          </form>
     </div>
   )
 }
