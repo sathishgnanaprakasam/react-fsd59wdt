804542c32ca39dd4b205fdda740f52371845cebc
githubscientist <sathish.datascientist@gmail.com>
2024-06-07 10:17:26 +0530

    game completed

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# React + Vite
+# TicTacToe Game
 
-This is a `React + Vite project` created for the batch `FSD59WD-T` to demonstrate the concepts of React.
+This is a simple TicTacToe game that can be played by two players. The winner is the first player to get three of their marks in a row, column, or diagonal.
+
+Starting an Empty board is displayed to the players. The players take turns to place their marks on the board. The game ends when a player wins or the board is full.
+
+![empty board](image.png)
+
+# How to play
+
+# How to use the project
+
+1. Clone the repository
+2. Run the following command to install the required dependencies:
+
+```bash
+npm install
+```
+
+3. Run the following command to start the game:
+
+```bash
+npm run dev
+```
+
+4. Open your browser and navigate to `http://localhost:5173/` to play the game.
+
+# Features of the game
+
+- The game can be played by two players.
+- The game ends when a player wins or the board is full.
+- The game displays the winner when a player wins.
+
+# Technologies used
+
+- HTML
+
+# Author
+
+# License
+
+# Future improvements
+
+- Add a feature to allow players to play against the computer.
+- Add a feature to allow players to choose their marks.
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,77 @@
-// Component: App
+import { useState } from 'react';
+import './App.css';
+
 const App = () => {
-  return <h1>Hello, World!</h1>
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    const newSquares = squares.slice();
+    
+    if (xIsNext) {
+      newSquares[i] = 'X';
+    } else {
+      newSquares[i] = 'O';
+    }
+
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    for (let i = 0; i < lines.length; i++){
+      const [a, b, c] = lines[i];
+
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
+
+  let status;
+  let winner = calculateWinner(squares);
+
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <h1>{ status }</h1>
+      <div>
+        <div className='row'>
+          <button className="square" onClick={() => handleClick(0)}>{ squares[0] }</button>
+          <button className="square" onClick={() => handleClick(1)}>{ squares[1] }</button>
+          <button className="square" onClick={() => handleClick(2)}>{ squares[2] }</button>
+        </div>
+        <div className='row'>
+          <button className="square" onClick={() => handleClick(3)}>{ squares[3] }</button>
+          <button className="square" onClick={() => handleClick(4)}>{ squares[4] }</button>
+          <button className="square" onClick={() => handleClick(5)}>{ squares[5] }</button>
+        </div>
+        <div className='row'>
+          <button className="square" onClick={() => handleClick(6)}>{ squares[6] }</button>
+          <button className="square" onClick={() => handleClick(7)}>{ squares[7] }</button>
+          <button className="square" onClick={() => handleClick(8)}>{ squares[8] }</button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
